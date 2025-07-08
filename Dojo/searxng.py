11ee7260ce98402502ae506922f97a633b181906
quentinL52 <158512396+quentinL52@users.noreply.github.com>
2025-07-08 13:30:36 +0200

    Delete Dojo directory

--- Dojo/searxng.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.utilities import SearxSearchWrapper
-search = SearxSearchWrapper(searx_host="http://localhost:8080")
-input = input("recherche : ")
-moteur = search.run(input)
-print(moteur)
